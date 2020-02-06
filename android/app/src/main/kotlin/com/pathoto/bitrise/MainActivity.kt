eced101cd75d91d3f2a9d109973507813e80bc74
Paul Cech <pc@plazz.ag>
2020-02-06 22:12:03 +0100

    first

---
+++ android/app/src/main/kotlin/com/pathoto/bitrise/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pathoto.bitrise
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
